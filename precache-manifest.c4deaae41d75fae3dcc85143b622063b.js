ae5d95083cc09b0d0cd3aa627b2614ba24dc3caf
gabriel <azb200@yahoo.com>
2018-12-13 14:25:51 -0500

    Updates

---
+++ precache-manifest.c4deaae41d75fae3dcc85143b622063b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/recipesfood/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "08056d07574482d0328c",
+    "url": "/recipesfood/static/css/main.29daa954.chunk.css"
+  },
+  {
+    "revision": "c96165c226ec31117b63d29641a80727",
+    "url": "/recipesfood/static/media/banner.c96165c2.png"
+  },
+  {
+    "revision": "9aaa5ca94214ac07e2e5",
+    "url": "/recipesfood/static/js/1.9aaa5ca9.chunk.js"
+  },
+  {
+    "revision": "c2ab22e09bc43a8c2fe0",
+    "url": "/recipesfood/static/js/runtime~main.c2ab22e0.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/recipesfood/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/recipesfood/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/recipesfood/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "08056d07574482d0328c",
+    "url": "/recipesfood/static/js/main.08056d07.chunk.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/recipesfood/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/recipesfood/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/recipesfood/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/recipesfood/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/recipesfood/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/recipesfood/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9aaa5ca94214ac07e2e5",
+    "url": "/recipesfood/static/css/1.29a37c2e.chunk.css"
+  },
+  {
+    "revision": "71be1bc6fb7b20a19c8accfa481f6a0e",
+    "url": "/recipesfood/index.html"
+  }
+];
